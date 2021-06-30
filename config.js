d4c747f843a9e97f88ce6f910664d9075094458f
haiyen221 <86719544+haiyen221@users.noreply.github.com>
2021-06-30 19:24:23 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,17 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    introTitle: 'Hello anh yêu của tuii ✌✌!',
+    introDesc: `Yêu nhau gần 8 tháng mà xa nhau gần 7 tháng rồi có buồn không ta??🤔🤔
+    Hỏi vậy thôi chứ yêu thương không hết lấy đâu ra buồn nhỉ !! 🙆‍♀️❤`,
+    btnIntro: 'Do you love me?',
+    title: 'Em nói anh cái này thôi nha',
+    desc: 'Nothing stays the same. It all gets crushes. It all gets broken. It all passed with time.Only the moment you are in has any meaning...Anh không cần hiểu rõ quá đâu. Chỉ cần hiểu là khoảnh khắc anh tồn tại trên thế giới này thật sự có ý nghĩa rồi',
+    btnYes: 'Yêu em <33',
+    btnNo: 'Nhảm lồn',
+    question:'Em yêu anh vừa đủ. Đủ để làm mọi thứ. Love you <3',
+    btnReply: 'Gửi cho em <3',
+    reply: 'Anh cũng yêu em nhiều <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    messDesc: '❤',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
