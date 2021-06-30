734af6481a86876eb7a193005b5da169d354283b
haiyen221 <86719544+haiyen221@users.noreply.github.com>
2021-06-30 19:40:51 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 const CONFIG = {
-    introTitle: 'Hello anh yêu của tuii ✌✌',
+    introTitle: 'Hello anh yêu của tuiiiii ✌✌',
     introDesc: `Yêu nhau gần 8 tháng mà xa nhau gần 7 tháng rồi có buồn không ta??🤔🤔
     Hỏi vậy thôi chứ yêu thương không hết lấy đâu ra buồn nhỉ !! 🙆‍♀️❤`,
     btnIntro: 'Do you love me?',
     title: 'Em nói với anh cái này thôi nha',
-    desc: 'othing stays the same.It all gets crushed. It all gets broken. It all passes with time. Only the moment you are in has any meaning...Anh không cần hiểu rõ quá đâu. Chỉ cần hiểu là Khoảnh khắc anh tồn tại trên thế giới này đã thật sự có ý nghĩa rồi ',
+    desc: 'Nothing stays the same.It all gets crushed. It all gets broken. It all passes with time. Only the moment you are in has any meaning...Anh không cần hiểu rõ quá đâu. Chỉ cần hiểu là Khoảnh khắc anh tồn tại trên thế giới này đã thật sự có ý nghĩa rồi ',
     btnYes: 'Yêu em <33',
     btnNo: 'nhảm lon',
     question:'Em yêu anh vừa đủ. Đủ để anh làm mọi thứ. Love you ❤',
